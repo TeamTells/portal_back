72ece21dfef0ae771c4dbfe02a5fadbc25c50ddd
Nikita <nik050502@yandex.ru>
2023-12-12 02:04:02 +0300

    Реализовал методы для объекта departmentRepository

--- company/impl/infrastructure/sql/DepartmentRepository.go
+++ company/impl/infrastructure/sql/DepartmentRepository.go
@@ -16,11 +16,58 @@ type departmentRepository struct {
 }
 
 func (r repository) GetDepartment(ctx context.Context, id int) (*domain.Department, error) {
+	query := `
+		SELECT department.id, department.name, department.parentdepartmentid, parentDepartment.name, 
+		       department.supervisorid, employeeaccount.firstname
+		FROM department
+		LEFT JOIN employeeaccount ON department.supervisorid = employeeaccount.id
+		LEFT JOIN department AS parentDepartment ON department.parentdepartmentid = department.id
+		WHERE department.id = $1
+	`
 
+	var departmentInfo *domain.Department
+	err := r.conn.QueryRow(ctx, query, departmentInfo).Scan(&departmentInfo.Id, &departmentInfo.Name, &departmentInfo.ParentDepartment.Id,
+		&departmentInfo.ParentDepartment.Name, &departmentInfo.Supervisor.Id, &departmentInfo.Supervisor.Name)
+	if err == pgx.ErrNoRows {
+		return departmentInfo, department.DepartmentEmployeesNotFound
+	} else if err != nil {
+		return departmentInfo, err
+	}
+
+	return departmentInfo, nil
 }
 
 func (r repository) GetDepartmentEmployees(ctx context.Context, departmentId int) ([]domain.Employee, error) {
+	query := `
+		SELECT employeeaccount.id, employeeaccount.firstname, employeeaccount.secondname, employeeaccount.surname,
+			employeeaccount.dateofbirth, auth_user.email
+		FROM employeeaccount
+		JOIN employee_department ON employeeaccount.id = employee_department.accountid
+		JOIN auth_user ON employeeaccount.userid = auth_user.id
+		WHERE employee_department.departmentid = $1
+	`
 
+	var departmentEmployees []domain.Employee
+	rows, err := r.conn.Query(ctx, query)
+	if err == pgx.ErrNoRows {
+		return departmentEmployees, department.DepartmentEmployeesNotFound
+	} else if err != nil {
+		return departmentEmployees, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var departmentEmployee domain.Employee
+		err := rows.Scan(&departmentEmployee.Id, &departmentEmployee.FirstName, &departmentEmployee.SecondName,
+			&departmentEmployee.Surname, &departmentEmployee.DateOfBirth, &departmentEmployee.Email,
+			&departmentEmployee.Icon, &departmentEmployee.TelephoneNumber)
+		if err != nil {
+			return departmentEmployees, err
+		}
+		departmentEmployees = append(departmentEmployees, departmentEmployee)
+	}
+
+	return departmentEmployees, nil
 }
 
 func (r repository) GetChildDepartments(ctx context.Context, id int) ([]domain.Department, error) {
@@ -32,6 +79,7 @@ func (r repository) GetChildDepartments(ctx context.Context, id int) ([]domain.D
 		LEFT JOIN department AS parentDepartment ON department.parentdepartmentid = department.id
 		WHERE department.parentdepartmentid = $1
 	`
+
 	var childDepartments []domain.Department
 	rows, err := r.conn.Query(ctx, query)
 	if err == pgx.ErrNoRows {
@@ -58,8 +106,9 @@ func (r repository) GetCountOfDepartmentEmployees(ctx context.Context, departmen
 	query := `
 		SELECT COUNT(*)
 		FROM employee_department
-		WHERE id = $1
+		WHERE departmentid = $1
 	`
+
 	var countOfDepartmentEmployees int
 	err := r.conn.QueryRow(ctx, query, departmentId).Scan(&countOfDepartmentEmployees)
 	if err == pgx.ErrNoRows {
@@ -78,6 +127,7 @@ func (r repository) GetRootCompanyDepartments(ctx context.Context, companyId int
 		LEFT JOIN employeeaccount ON department.supervisorid = employeeaccount.id
 		WHERE department.companyid = $1 AND department.parentdepartmentid = NULL
 	`
+
 	var rootCompanyDepartments []domain.Department
 	rows, err := r.conn.Query(ctx, query)
 	if err == pgx.ErrNoRows {
