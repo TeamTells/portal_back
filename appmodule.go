c7599c137ea7b13ff0a62b04787016609d7176c8
Алексей Замятин <79986929+RoboutGulman@users.noreply.github.com>
2023-12-25 18:47:00 +0300

    обновил readme и appmodule

--- appmodule.go
+++ appmodule.go
@@ -2,35 +2,71 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
 	"log"
 	"net/http"
 	"os"
 	authcmd "portal_back/authentication/cmd"
-	di2 "portal_back/documentation/impl/di"
+	"portal_back/authentication/impl/di"
+	companyDi "portal_back/company/impl/di"
+	documentationDi "portal_back/documentation/impl/di"
+	rolesDi "portal_back/roles/impl/di"
 )
 
 func InitAppModule() {
-	authService, authConn, err := authcmd.InitAuthModule(authcmd.NewConfig())
-	if err != nil {
-		log.Fatal("failed init auth module:", err)
-	}
-	defer authConn.Close(context.Background())
+	conn := createConnection()
+	defer conn.Close(context.Background())
 
-	documentConnection := di2.InitDocumentModule(authService)
+	authService, userRequestService := di.InitAuthModule(conn)
+
+	documentConnection := documentationDi.InitDocumentModule(authService)
 	defer documentConnection.Close(context.Background())
 
 	// можно инжектить в другие модули
 	authService.IsAuthenticated("")
+
+	rolesModule := rolesDi.InitRolesModule()
+
+	companyDi.InitCompanyModule(authService, userRequestService, rolesModule, conn)
 
 	appPort := os.Getenv("BACKEND_PORT")
 	if appPort == "" {
 		appPort = "8080"
 	}
 
-	err = http.ListenAndServe(":"+appPort, nil)
+	err := http.ListenAndServe(":"+appPort, nil)
 	if err != nil {
 		log.Panic("ListenAndServe: ", err)
 	}
+}
+
+func createConnection() *pgx.Conn {
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		dbUser = "postgres"
+	}
+
+	dbPassword := os.Getenv("DB_PASSWORD")
+	if dbPassword == "" {
+		dbPassword = "password"
+	}
+
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		dbName = "app"
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "localhost"
+	}
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:5432/%s", dbUser, dbPassword, dbHost, dbName)
+
+	conn, _ := pgx.Connect(context.Background(), connStr)
+
+	return conn
 }
 
 func migrate() {
